a5642a985a3893d9cbcd5f012a5770ddbc1abe35
SteveBenz <stbenz@microsoft.com>
2022-07-02 19:07:11 -0700

    Gave bullets a lifetime

--- bullet.py
+++ bullet.py
@@ -3,17 +3,21 @@ from coordinateSpace import Velocity
 import pygame
 from pygame.event import Event
 from pygame.surface import Surface
+import time
+import userEvents
 
 class Bullet:
     def __init__(self, window: Surface, point: GamePoint, velocity: Velocity):
         self.__window = window
         self.__position = point
         self.__velocity = velocity
-        self.__endTime = 3 # TODO: Now + something
+        self.__endTime = time.time() + 3 #  Our bullets last 3 seconds
 
     def update(self, events: list[Event]) -> None:
         self.__position = self.__velocity.move(self.__position)
         self.__draw()
+        if time.time() > self.__endTime:
+            pygame.event.post(Event(userEvents.BULLET, **{'bullet': self, 'action': 'delete'}))
     
     def __draw(self) -> None:
         (cx, cy) = mapPoint(self.__position, self.__window)
